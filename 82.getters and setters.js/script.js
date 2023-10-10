8f303459fda44b1ef2ac165130c396def286f7e5
hackerbotsupreme <129755834+hackerbotsupreme@users.noreply.github.com>
2023-10-10 18:14:04 +0530

    Add files via upload

---
+++ 82.getters and setters.js/script.js
@@ -0,0 +1,18 @@
+class Animal {
+    constructor(name) {
+      this.name = Animal.capitalize(name)
+    }
+    walk() {
+      alert("Animal " + this.name + " is walking")
+    }
+    static capitalize(name) {
+      return name.charAt(0).toUpperCase() + name.substr(1, name.length)
+    }
+  }
+  
+  j = new Animal("jack")
+  j.walk()
+  // console.log(j.capitalize("thisa")) // --- > this doesnt work
+  
+  
+  
